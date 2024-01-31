96d9501b8a9400d81b4d7e0b61a4e92270c3feb4
Noah Bazer <nbazer00@gmail.com>
2024-01-30 21:32:47 -0600

    fix class traffic light

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,44 @@
 import { Component } from "react";
 
-export class ClassTrafficLight extends Component {
+interface State {
+  activeLight: string;
+}
+
+export class ClassTrafficLight extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      activeLight: 'red',
+    };
+  }
+
+  changeLight = () => {
+    let nextLight;
+    switch (this.state.activeLight) {
+      case 'red':
+        nextLight = 'green';
+        break;
+      case 'green':
+        nextLight = 'yellow';
+        break;
+      case 'yellow':
+      default:
+        nextLight = 'red';
+        break;
+    }
+    this.setState({ activeLight: nextLight });
+  }
+
   render() {
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${this.state.activeLight === 'red' ? 'red' : 'black'}`}></div>
+          <div className={`circle ${this.state.activeLight === 'yellow' ? 'yellow' : 'black'}`}></div>
+          <div className={`circle ${this.state.activeLight === 'green' ? 'green' : 'black'}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.changeLight}>Next State</button>
       </div>
     );
   }

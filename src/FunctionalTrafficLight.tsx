9640eeacff5e168c997dbb06d62b4e22db10c251
Noah Bazer <nbazer00@gmail.com>
2024-01-30 22:31:08 -0600

    fix functional traffic light

--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
+
 export const FunctionalTrafficLight = () => {
+  const [activeLight, setActiveLight] = useState<'red' | 'green' | 'yellow'>('red');
+
+  const changeLight = () => {
+    let nextLight: 'red' | 'green' | 'yellow';
+    switch (activeLight) {
+      case 'red':
+        nextLight = 'green';
+        break;
+      case 'green':
+        nextLight = 'yellow';
+        break;
+      case 'yellow':
+      default:
+        nextLight = 'red';
+        break;
+    }
+    setActiveLight(nextLight);
+  }
+  
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${activeLight === 'red' ? 'red' : 'black'}`}></div>
+        <div className={`circle ${activeLight === 'yellow' ? 'yellow' : 'black'}`}></div>
+        <div className={`circle ${activeLight === 'green' ? 'green' : 'black'}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={changeLight}>Next State</button>
     </div>
   );
 };
